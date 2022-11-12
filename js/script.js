c5527c5472e4f21a9055791a9b023397e540a48e
Alex <zabrodski666@gmail.com>
2022-11-12 17:06:32 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let number = 1;
+
+number = 10;
+
+
+// console.log(number(x, n));
+
+// const yo = confirm('Are you here?');
+// console.log(yo);
+
+const answer = prompt('Are you 18 years old?', '18');
+console.log(typeof (answer));
